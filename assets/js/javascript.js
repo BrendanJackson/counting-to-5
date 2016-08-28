52cfd16a25355d8ae8150c543f94a56085b60b39
BrendanJackson <bajackson859@gmail.com>
2016-08-27 20:37:40 -0400

    initial commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,32 @@
+$(function(){
+    //waits 3 seconds before running anything
+    setTimeout(function(){
+
+        $('body').append('<div class="test"></div>');
+        $('.test').addClass('animation');
+        $('.animation').on('webkitAnimationEnd oanimationend oAnimationEnd msAnimationEnd animationend',
+            function(e){
+                //clicked it waits 3 seconds before disappearing
+                if($('.test').hasClass('clicked')){
+                    $('.test').fadeOut(3000, function(){
+                        $(this).remove();
+                    });
+                } else {
+                    //clicked it waits 1 second before disappearing
+                    $('.test').fadeOut(1000, function(){
+                        $(this).remove();
+                    });
+                }
+            });
+
+
+    }, 3000);
+});
+
+
+
+$(document).on({
+    click: function(e) {
+        $(this).addClass('clicked');
+    }
+}, '.test');
